04ace7ddde655d5a295741a44e279cc4e8fb933f
creativish <105737290+creativish@users.noreply.github.com>
2022-05-20 09:02:00 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+
+resp = requests.get(url="http://api.open-notify.org/iss-now.json")
+resp.raise_for_status()
+
+data = resp.json()
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+iss_position = (longitude, latitude)
+print(iss_position)
